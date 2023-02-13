a32ceaba0ecf185c3620db75bf38212d1a11be36
Amikan93 <Amikan93@AMIKAN>
2023-02-13 16:47:21 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ASP.NET.HomeWork._03.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NET.HomeWork._03.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public string Hello()
+        {
+            return "Hello ASP.NET Core MVC";
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/ShowOS.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET.HomeWork._03.Controllers
+{
+    public class ShowOS : Controller
+    {
+        public IActionResult GetOperatingSystem()
+        {
+            string operatingSystem = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            return Content($"Server operating system is {operatingSystem}");
+        }
+    }
+}
